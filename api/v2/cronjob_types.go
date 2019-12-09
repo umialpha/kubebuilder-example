01d94b99c982418dc90b13fdcedded21d9900ac8
roylee <roy236977634@gmail.com>
2019-12-09 06:30:18 +0000

    multiversion config

--- api/v2/cronjob_types.go
+++ api/v2/cronjob_types.go
@@ -48,7 +48,6 @@ the fields.
 
 // CronJobSpec defines the desired state of CronJob
 type CronJobSpec struct {
-	// +kubebuilder:validation:MinLength=0
 
 	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
 	Schedule CronSchedule `json:"schedule"`
